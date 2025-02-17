01b19c83ccc7260e0d99ddf7ee2134375dbcfefe
Donald Toomey <198673685+D-O-N-A-L-D@users.noreply.github.com>
2025-02-17 17:33:16 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `CHAT WITH DONBOT`;
+export const CHAT_HEADER: string = `Chat with DonBot, an AI Bot created by Donald Toomey`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `©2025 Donald Toomey`;
 export const CLEAR_BUTTON_TEXT: string = `Clear our Conversation`;
-export const PAGE_TITLE: string = `Chat with DonBot, an AI Bot by Donald Toomey`;
+export const PAGE_TITLE: string = `DonBot`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant. I'm here to help you learn about Donald and his previous experiences.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
