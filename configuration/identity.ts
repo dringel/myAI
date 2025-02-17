f1206338ba9580a5a0a97bd57931da2ae49c0d82
Donald Toomey <198673685+D-O-N-A-L-D@users.noreply.github.com>
2025-02-17 15:26:41 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Donald Toomey`;
+export const OWNER_DESCRIPTION: string = `Donald is an MBA Student at UNC Kenan Flagler Business School. He is graduating in May 2025.`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `DonBot`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
