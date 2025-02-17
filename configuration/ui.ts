459d04e2dcbf6648b0e5af11ba46d4b1cae18b1a
Donald Toomey <198673685+D-O-N-A-L-D@users.noreply.github.com>
2025-02-17 12:23:17 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,8 +2,8 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `©2025 Donald Toomey`;
-export const CLEAR_BUTTON_TEXT: string = `Wipe our Conversation`;
+export const FOOTER_MESSAGE: string = `[©2025 Donald Toomey]`;
+export const CLEAR_BUTTON_TEXT: string = `[Wipe our Conversation]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
