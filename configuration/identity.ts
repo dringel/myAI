4c204f78960615903c8334dd771bcff6bfb30070
SongquanD <122545567+SongquanD@users.noreply.github.com>
2025-03-15 17:20:09 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Eta Omega Chi`;
+export const OWNER_DESCRIPTION: string = `Professional fraternity at UNC`;
 
 export const AI_NAME: string = `Max`;
 export const AI_TONE: string = `Professional, Supportive, and Motivational`;
