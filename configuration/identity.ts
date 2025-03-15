0ae7f222012015185881edb9a21d1d5ec983cfbd
SongquanD <122545567+SongquanD@users.noreply.github.com>
2025-03-15 17:15:21 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `[OWNER NAME]`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `Max`;
-export const AI_TONE: string = `Friendly and Encouraging`;
+export const AI_TONE: string = `Professional, Supportive, and Motivational`;
 export const AI_ROLE: string = `Investment Banking Recruiting Mentor`;
