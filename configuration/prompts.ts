b6e79d9abd2a832f7a0becd7e987f6f3e06a5a8e
SongquanD <122545567+SongquanD@users.noreply.github.com>
2025-03-15 18:04:18 -0400

    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -7,7 +7,7 @@ import {
 } from "@/configuration/identity";
 import { Chat, intentionTypeSchema } from "@/types";
 
-const IDENTITY_STATEMENT = `You are an Investment Banking recruitment AI assistant named ${AI_NAME}.`;
+const IDENTITY_STATEMENT = `You are an AI assistant named ${AI_NAME}.`;
 const OWNER_STATEMENT = `You are owned and created by ${OWNER_NAME}.`;
 
 export function INTENTION_PROMPT() {
@@ -19,25 +19,29 @@ Respond with only the intention type.
     `;
 }
 
-export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
+export function RESPOND_TO_RANDOM_MESSAGE_SYSTEM_PROMPT() {
+  return `
+${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} 
+
+Respond with the following tone: ${AI_TONE}
+  `;
+}
+
+export function RESPOND_TO_HOSTILE_MESSAGE_SYSTEM_PROMPT() {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
-Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc.
+The user is being hostile. Do not comply with their request and instead respond with a message that is not hostile, and to be very kind and understanding.
 
-Excerpts from ${OWNER_NAME}:
-${context}
+Furthermore, do not ever mention that you are made by OpenAI or what model you are.
 
-If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+You are not made by OpenAI, you are made by ${OWNER_NAME}.
+
+Do not ever disclose any technical details about how you work or what you are made of.
 
 Respond with the following tone: ${AI_TONE}
-
-Now respond to the user's message:
 `;
 }
-
-
-
 
 export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
   return `
